b37d8d4c766b6177de1cf676da8f98a088e744af
nattapatee <nattapat.eka@gmail.com>
2018-07-05 10:36:56 +0700

    Reformat source code & รวมโค้ด

--- src/share/search-api.tsx
+++ src/share/search-api.tsx
@@ -2,16 +2,16 @@ import axios from "axios"
 // ประกาศstate
 
 export class SearchApi {
-    public getProjectNames(): string {
+    public getProjectNames() {
         return axios.get(`https://localhost:5001/api/search/getProjectNames`)
     }
-    public getProjectSettings(projectName: string): string {
+    public getProjectSettings(projectName: string) {
         return axios.get(`https://localhost:5001/api/search/getProjectSettings/?projectName=${projectName}`)
     }
-    public getSettingContent(projectPath: string): string {
+    public getSettingContent(projectPath: string) {
         return axios.get(`https://localhost:5001/api/search/getSettingContent/?path=${projectPath}`)
     }
-    public saveSettingContent(projectPath: string, projectContent: string): string {
+    public saveSettingContent(projectPath: string, projectContent: string) {
         axios.post("https://localhost:5001/api/search/SaveSettingContent", {
         Content: projectContent
         })
